f9070fe22948d1db7b518a95bf8ff7e8e18d4bf5
Patrick Lorio <patrick@methods.dev>
2024-08-08 11:28:31 -0700

    Release 0.15.18

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "${SCRIPT_DIR}/../data"
 
-gpg -a -o Release.pgp --detach-sig Release
-gpg -a -o InRelease --clearsign Release
+gpg --local-user DA6CD75EC2839680 -a -o Release.pgp --detach-sig Release
+gpg --local-user DA6CD75EC2839680 -a -o InRelease --clearsign Release
